7a7e3be475ec867a79cad8c26152a440ad75b8b2
Javier Antiles <javier_susy_nike@yahoo.com>
2018-01-18 00:16:35 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,7 @@ end
 # code your input_to_index and move method here!
 def input_to_index(index = 0)
   "index".to_i
-  index =- 1
+  index = index - 1
 end
 
 def update_array_at_with(array,index,choice)
